9375d76f26be30604045fe918a9f2ad7a88629a1
yunaranyancat <mzulfiqar.wardi@gmail.com>
2019-03-22 20:11:43 +0100

    Hello world
    welcome to the world of Gophers

---
+++ hello.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello yunaranyancat,", "how are you?")
+	fmt.Println("To print your GOPATH : go env GOPATH,")
+	fmt.Println("To compile GO code to work with different environement : https://golang.org/doc/install/source#environment.")
+	fmt.Print("Print only no new line", "ofc")
+}
